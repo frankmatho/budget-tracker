570177e15e944a436f1597db869c1eb053d68e4c
ftotherank <102555981+ftotherank@users.noreply.github.com>
2026-02-02 12:06:05 +0300

    day 8 working

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
 import { useState } from 'react'
+import { useEffect } from 'react'
 import './App.css'
 import Navbar from './components/navbar.jsx'
 //import BalanceCard from './components/details.jsx'
 import Balance from './components/balance.jsx'
 
 function App() {
-  const [budget, setBudget] = useState(0);
-  const [expenses, setExpenses] = useState([]);
-  const [hasBudget, setHasBudget] = useState(false);
+  const [budget, setBudget] = useState(() =>{
+    return Number(localStorage.getItem("budget")) || 0;
+  });
+
+  const [expenses, setExpenses] = useState(() =>{
+    const saved = localStorage.getItem("expenses");
+    return saved ? JSON.parse(saved) : [];
+  });
+
+  const [hasBudget, setHasBudget] = useState(() =>{
+    return localStorage.getItem("hasBudget") === "true";
+  });
+
+  useEffect(() => {
+    localStorage.setItem("budget", budget);
+  }, [budget]);
+
+  useEffect(() => {
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+  }, [expenses]);
+
+  useEffect(() => {
+    localStorage.setItem("hasBudget", hasBudget);
+  }, [hasBudget]);
 
   return (
     <div className="min-h-screen bg-gray-100">
